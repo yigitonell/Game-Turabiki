b44947f7dff4a54523bfe516fb6357378bcab45d
yigitonell <onelkamilyigit@gmail.com>
2025-02-22 16:27:08 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,47 @@
+print(r'''
+*******************************************************************************⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡖⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢹⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡄⠤⠤⠄⠤⠠⠤⠤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠸⣭⣿⠊⠁⠀⠀⠀⠀⠙⠲⢄⣀⣀⣀⣀⣀⠤⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣞⣁⡾⠤⠤⡀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠘⡆⠀⠀⡫⠋⠉⠉⣉⣉⣲⡄⠀⠀⢠⡀⢀⣀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⡇⠀⠀⡇⠀⠀⡞⠁⠀⠀⠀⠀⣤⣞⣣⣕⠁⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⡰⢦⣇⣠⠖⠋⠀⠀⠀⢱⡀⠀⣇⠀⠀⠀⠀⠀⠀⣨⠚⠻⡆⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢹⠚⣧⠀⠀⠀⠀⠀⠀⠀⠙⢦⣈⣓⣒⣒⡆⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⠉⡏⣇⠀⠀⠀⠀⢀⡴⠚⠉⠉⠒⠲⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡎⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡇⠈⠓⠒⠒⠊⠉⠀⠀⠀⠀⠀⠀⠱⣄⠀⠀⠀⠀⠀⠀⠀⠀⡠⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⠢⠤⠤⠤⠖⠚⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢀⡀⣀⣄⡀⣀⣀⣀
+⠤⠤⠤⠤⣄⡀⣀⣇⣀⣀⣀⣀⣀⡠⠤⠤⠤⠖⠂⠒⠒⠒⠒⠒⠒⠒⠒⠂⠒⠐⠒⠂⠒⠐⠂⠂⠐⠒⠚⠒⠛⠒⠂⠒⠊⠉⠁⠁⠑⠀
+*******************************************************************************
+''')
+print("Welcome to the Turkiye server.")
+print('Rules are simple for choice just type inside of the " " and it will ok')
+print("You need to stop Turbo Turabi!!!.")
+first_choice = input('You heard some song with pervert lyrics and rhythmic tones,\n U have two ways where you go?'
+                     ' "left" or "right"\n').lower()
+if first_choice == "right":
+    second_choice = input('You chose Allah\'s way so you are on true way, '
+                          'You walked a while and saw a Grandma asking for the way of Taksim, what you will do?'
+                          ' "give" her navigations or just "shake" your head in the silence\n').lower()
+    if second_choice == "shake":
+        third_choice = input('You don\'t break your mewing streak u are the best,'
+                             'And you arrived TURABI boss\'s castle what you will do? '
+                             'enter "turquoise" door, enter "black" door or enter "red" door\n').lower()
+        if third_choice == "turquoise":
+            print("You entered TURABI's room and slapped his badass beard with ottoman slap, You WIN!!!")
+        elif third_choice == "brlack":
+            print("You saw some black people and they are angry\nbecause someone asked them are you a watch seller\nthey are mad and fight with you, Game Over.")
+        else:
+            print("You opened the door from GOP and got stabbed, u died Game Over.")
+
+    else:
+        print("You give navigations but she is a witch and tricks you, You dead, game is over.")
+else:
+    print("U chose the left so u accidentally fell into greek sea, Game Over.")
